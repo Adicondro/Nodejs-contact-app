61dd7d502f8b52c07598b78b1fe6a3dd6b6b77eb
Adicondro Yusuf <adicondro.yusuf@gmail.com>
2024-08-10 01:55:34 +0700

    Solving Callback Hell

--- app.js
+++ app.js
@@ -6,24 +6,47 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
-rl.question('Masukkan Nama Anda : ', (nama) => {
-    rl.question('Masukkan No Hp Anda : ', (noHP) => {
-        
-        const contact = {
-            nama,
-            noHP,
-        };
-        const file = fs.readFileSync('data/contacts.json', 'utf-8')
-        const contacts = JSON.parse(file);
-        
-        contacts.push(contact);
-        
-        fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
-        
-        console.log(contacts)
-        console.log("Terimakasih atas Partisipasinya")
+const dirPath = './data';
+if(!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+}
 
-        rl.close();
+// Membuat fo\ile contacts.json jika belum ada
+const dataPath = './data/contacts.json'
+if(!fs.existsSync(dataPath)) {
+    fs.writeFileSync(dataPath, '[]', 'utf-8')
+}
+
+
+const setPertanyaan = (pertanyaan) => {
+    return new Promise((resolve, reject) => {
+        rl.question(pertanyaan, (nama) => {
+            resolve(nama);
+        })
     })
-})
+}
+
+
+const main = async () => {
+    const nama = await setPertanyaan("Masukkan Nama Anda : ");
+    const email = await setPertanyaan("Masukkan Email Anda : ");
+    const noHP = await setPertanyaan("Masukkan Nomor HP Anda : ");
+
+    const contact = { nama, email, noHP };
+    const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8')
+    const contacts = JSON.parse(fileBuffer);
+            
+    contacts.push(contact);
+            
+    fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
+            
+    console.log(contacts)
+    console.log("Terimakasih atas Partisipasinya")
+    
+    rl.close();
+};
+
+
+main()
+
 
